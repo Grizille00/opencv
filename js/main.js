565beeb2827bafd0c18538493483b7db14c84209
Grizille00 <grizille34@gmail.com>
2021-09-11 11:45:41 +0200

    main2

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ $(document).on('ready',()=>{
         centerPadding: '60px',
         slidesToShow: 3,
         slidesToScroll:1,
-        // autoplay:true,
+        autoplay:true,
         autoplaySpeed:3000,
         arrows:false,
         infinite: true,
@@ -50,8 +50,8 @@ $(document).on('ready',()=>{
         $('.funder').slick({
             // centerMode: true,
             centerPadding: '60px',
-            slidesToShow: 4,
-            slidesToScroll:1,
+            slidesToShow: 3,
+            slidesToScroll:3,
             autoplay:true,
             autoplaySpeed:3000,
             arrows:true,
@@ -63,19 +63,22 @@ $(document).on('ready',()=>{
                     breakpoint: 1025,
                     settings: {
                       slidesToShow: 2,
+                      slidesToScroll:2,
                     }
                   },
                 {
                 breakpoint: 992,
                 settings: {
                   slidesToShow: 2,
+                  slidesToScroll:1,
+                  centerPadding: '20px',
                 }
               },
               {
                 breakpoint: 576,
                 settings: {
                   slidesToShow: 1,
-                  centerPadding: '10px',
+                  centerPadding: '60px',
                   centerMode:true,
                 }
               }
@@ -92,20 +95,18 @@ $(document).on('ready',()=>{
 const btns = document.querySelectorAll(".faq-qsn .title")
 const ans = document.querySelectorAll(".faq-qsn .ans")
 const imgs = [
-    "img/Icon feather-plus.png",
-    "img/Icon feather-minus.png"
+  "bi-plus-lg",
+  "bi-dash-lg"
 ]
 for (let i = 0; i < btns.length; i++) {
-    btns[i].children[1].children[0].setAttribute('src',imgs[0])
-    btns[i].addEventListener('click',()=>{
-        ans[i].classList.toggle('open')
-        if(btns[i].children[1].children[0].getAttribute('src') == imgs[0]) {
-            btns[i].children[1].children[0].setAttribute('src',imgs[1])
-        } else{
-            btns[i].children[1].children[0].setAttribute('src',imgs[0])
-        }
-    })
-    
+  btns[i].children[1].classList.toggle(imgs[0])
+  btns[i].addEventListener('click',()=>{
+      ans[i].classList.toggle('open')
+      btns[i].children[1].classList.toggle(imgs[0])
+      btns[i].children[1].classList.toggle(imgs[1])
+     
+  })
+  
 }
 
 // Accordion End
