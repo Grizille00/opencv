2f66374b7eb761b5a531b5dc9a0b5481dfffd8f9
Grizille00 <grizille34@gmail.com>
2021-09-15 09:11:34 +0200

    sub5

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ const nav = document.querySelector(".menu");
 
 
 // Events
-hum.addEventListener("click", ()=>{
+hum.addEventListener("click", () => {
     hum.classList.toggle("open");
     nav.classList.toggle("open");
 })
@@ -17,73 +17,71 @@ hum.addEventListener("click", ()=>{
 
 
 // Slick 
-$(document).on('ready',()=>{
+$(document).on('ready', () => {
     $('.center').slick({
         // centerMode: true,
         centerPadding: '90px',
         slidesToShow: 3,
-        slidesToScroll:1,
+        slidesToScroll: 1,
         // autoplay:true,
-        autoplaySpeed:3000,
-        arrows:false,
+        autoplaySpeed: 3000,
+        arrows: false,
         infinite: true,
-        responsive: [
-          {
-            breakpoint: 992,
-            settings: {
-              slidesToShow: 2,
-            }
-          },
-          {
-            breakpoint: 576,
-            settings: {
-              slidesToShow: 1,
-              centerPadding: '20px',
-              centerMode:true,
-            }
-          }
-
-        ]
-        });
-
-        $('.funder').slick({
-            // centerMode: true,
-            centerPadding: '60px',
-            slidesToShow: 3,
-            slidesToScroll:2,
-            autoplay:true,
-            autoplaySpeed:3000,
-            arrows:true,
-            infinite: true,
-            prevArrow:".btnPrev",
-            nextArrow:".btnNext",
-            responsive: [
-                {
-                    breakpoint: 1025,
-                    settings: {
-                      slidesToShow: 2,
-                      slidesToScroll:2,
-                    }
-                  },
-                {
+        responsive: [{
                 breakpoint: 992,
                 settings: {
-                  slidesToShow: 3,
-                  slidesToScroll:1,
-                  centerPadding: '60px',
+                    slidesToShow: 2,
                 }
-              },
-              {
+            },
+            {
                 breakpoint: 576,
                 settings: {
-                  slidesToShow: 3,
-                  centerPadding: '20px',
-                  centerMode:true,
+                    slidesToShow: 1,
+                    centerPadding: '20px',
+                    centerMode: true,
                 }
-              }
-    
-            ]
-            });
+            }
+
+        ]
+    });
+
+    $('.funder').slick({
+        // centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: true,
+        infinite: true,
+        prevArrow: ".btnPrev",
+        nextArrow: ".btnNext",
+        responsive: [{
+                breakpoint: 1025,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    centerPadding: '60px',
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 3,
+                    centerPadding: '20px',
+                    centerMode: true,
+                }
+            }
+
+        ]
+    });
 })
 
 // Slick End
@@ -94,33 +92,33 @@ $(document).on('ready',()=>{
 const btns = document.querySelectorAll(".faq-qsn .title")
 const ans = document.querySelectorAll(".faq-qsn .ans")
 const imgs = [
-  "bi-plus-lg",
-  "bi-dash-lg"
+    "bi-plus-lg",
+    "bi-dash-lg"
 ]
 for (let i = 0; i < btns.length; i++) {
-  btns[i].children[1].classList.toggle(imgs[0])
-  btns[i].addEventListener('click',()=>{
-      ans[i].classList.toggle('open')
-      btns[i].children[1].classList.toggle(imgs[0])
-      btns[i].children[1].classList.toggle(imgs[1])
-     
-  })
-  
+    btns[i].children[1].classList.toggle(imgs[0])
+    btns[i].addEventListener('click', () => {
+        ans[i].classList.toggle('open')
+        btns[i].children[1].classList.toggle(imgs[0])
+        btns[i].children[1].classList.toggle(imgs[1])
+
+    })
+
 }
 
 // Accordion End
 
 
 $(window).on("load", function() {
-  $('#preloader').fadeOut(500);
-  $('#main-wrapper').addClass('show');
+    $('#preloader').fadeOut(500);
+    $('#main-wrapper').addClass('show');
 
-  // if($('body').attr('data-sidebar-position') === "fixed") {
-  //     $('.quixnav-scroll').slimscroll({
-  //         position: "right",
-  //         size: "5px",
-  //         height: "100%",
-  //         color: "transparent"
-  //     });
-  // }
+    // if($('body').attr('data-sidebar-position') === "fixed") {
+    //     $('.quixnav-scroll').slimscroll({
+    //         position: "right",
+    //         size: "5px",
+    //         height: "100%",
+    //         color: "transparent"
+    //     });
+    // }
 });
